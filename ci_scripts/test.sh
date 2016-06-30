5a09cf689b4df94f96f1dd196597625d9b97d3d2
arokem <arokem@gmail.com>
2016-06-29 19:11:58 -0700

    New CI scripts, based on scikit-learn-contrib project template.

---
+++ ci_scripts/test.sh
@@ -0,0 +1,13 @@
+set -e
+
+# Get into a temp directory to run test from the installed scikit learn and
+# check if we do not leave artifacts
+mkdir -p $TEST_DIR
+
+cd $TEST_DIR
+
+if [[ "$COVERAGE" == "true" ]]; then
+    py.test --pyargs $MODULE --cov-report term-missing --cov=$MODULE
+else
+    py.test --pyargs $MODULE
+fi
