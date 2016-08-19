64f7ebc9d671d05d510d2668007e8edabfd744bf
arokem <arokem@gmail.com>
2016-08-19 16:16:31 -0700

    Notebooks and examples.

--- examples/plot_afq_api.py
+++ examples/plot_afq_api.py
@@ -6,9 +6,10 @@ AFQ API
 An example using the AFQ API
 
 """
+import os.path as op
 
 import matplotlib.pyplot as plt
-import os.path as op
+import nibabel as nib
 
 from AFQ import api
 import AFQ.data as afd
@@ -18,6 +19,9 @@ base_dir = op.join(op.expanduser('~'), 'AFQ_data', 'stanford_hardi')
 myafq = api.AFQ(preproc_path=op.join(afd.afq_home, 'stanford_hardi'),
                 sub_prefix='sub')
 
+FA = nib.load(myafq.dti_fa[0]).get_data()
+
 fig, ax = plt.subplots(1)
-ax.matshow(myafq.dti[0].fa[:, :, 40])
+ax.matshow(FA[:, :, FA.shape[-1]//2], cmap='viridis')
 ax.axis("off")
+plt.show()
