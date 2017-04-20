3739120dd891c282e8cc171b8ab468e1db6651c9
Rafael Neto Henriques <rafaelnh21@gmail.com>
2017-04-20 15:44:55 -0700

    NF: Add DKI microstructural model fit in dki.py

--- AFQ/dki.py
+++ AFQ/dki.py
@@ -6,6 +6,7 @@ import numpy as np
 import nibabel as nib
 
 from dipy.reconst import dki
+from dipy.reconst import dki_micro
 from ._fixes import dki_prediction
 
 import AFQ.utils.models as ut
@@ -235,6 +236,80 @@ def fit_mdki(data_files, bval_files, bvec_files, mask=None, out_dir=None,
         nib.save(nib.Nifti1Image(m, aff), file_paths[n])
 
     return file_paths
+
+
+def fit_dkimicro(data_files, bval_files, bvec_files, mask=None,
+                 min_kurtosis=-1, max_kurtosis=3, out_dir=None,
+                 b0_threshold=0):
+    """
+    Fit the DKI model, save files with derived maps
+
+    Parameters
+    ----------
+    data_files : str or list
+        Files containing DWI data. If this is a str, that's the full path to a
+        single file. If it's a list, each entry is a full path.
+    bval_files : str or list
+        Equivalent to `data_files`.
+    bvec_files : str or list
+        Equivalent to `data_files`.
+    mask : ndarray, optional
+        Binary mask, set to True or 1 in voxels to be processed.
+        Default: Process all voxels.
+    min_kurtosis : float, optional
+        The minimal plausible value of kurtosis. Default: -1.
+    max_kurtosis : float, optional
+        The maximal plausible value of kurtosis. Default: 3.
+    out_dir : str, optional
+        A full path to a directory to store the maps that get computed.
+        Default: maps get stored in the same directory as the last DWI file
+        in `data_files`.
+    b0_threshold : float
+
+
+    Returns
+    -------
+    file_paths : a dict with the derived maps that were computed and full-paths
+    to the files containing these maps.
+
+    Note
+    ----
+    Maps that are calculated: FA, MD, AD, RD, MK, AK, RK
+
+    """
+    img, data, gtab, mask = ut.prepare_data(data_files, bval_files,
+                                            bvec_files, mask=mask,
+                                            b0_threshold=b0_threshold)
+
+    dkimodel = dki_micro.KurtosisMicrostructureModel(gtab)
+    dkifit = dkimodel.fit(data, mask=mask)
+
+    AWF = dkifit.awf
+    T = dkifit.tortuosity
+    Da = dkifit.axonal_diffusivity
+    hRD = dkifit.hindered_rd
+    hAD = dkifit.hindered_ad
+    evals = dkifit.hindered_evals
+    hMD = (evals[..., 0] + evals[..., 1] + evals[..., 2]) / 3.0
+    params = dkifit.model_params
+
+    maps = [AWF, T, hAD, hRD, hMD, Da, params]
+    names = ['AWF', 'T', 'hAD', 'hRD', 'hMD', 'Da', 'params']
+
+    if out_dir is None:
+        out_dir = op.join(op.split(data_files)[0], 'dkimicro')
+
+    if not op.exists(out_dir):
+        os.makedirs(out_dir)
+
+    aff = img.get_affine()
+    file_paths = {}
+    for m, n in zip(maps, names):
+        file_paths[n] = op.join(out_dir, 'dkimicro_%s.nii.gz' % n)
+        nib.save(nib.Nifti1Image(m, aff), file_paths[n])
+
+    return file_paths
+
 
 
 def predict(params_file, gtab, S0_file=None, out_dir=None):
