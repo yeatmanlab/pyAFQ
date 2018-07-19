b5a9be7f20333d0bf309ab654a6c46ee59e4a357
Ariel Rokem <arokem@gmail.com>
2018-07-19 17:26:21 -0400

    First pass at full interactive visualization.

--- AFQ/viz.py
+++ AFQ/viz.py
@@ -5,7 +5,7 @@ import IPython.display as display
 
 import nibabel as nib
 from dipy.viz.colormap import line_colors
-from dipy.viz import window, actor
+from dipy.viz import window, actor, ui
 import dipy.tracking.utils as dtu
 import dipy.tracking.streamline as dts
 
@@ -143,25 +143,141 @@ def visualize_volume(volume, x=None, y=None, z=None, ren=None, inline=True,
     """
     Visualize a volume
     """
-    if x is None:
-        x = volume.shape[0] // 2
-    if y is None:
-        y = volume.shape[1] // 2
-    if z is None:
-        z = volume.shape[2] // 2
-
-    vol_actor1 = actor.slicer(volume)
-    vol_actor1.display(x=x)
-    vol_actor2 = vol_actor1.copy()
-    vol_actor2.display(y=y)
-    vol_actor3 = vol_actor1.copy()
-    vol_actor3.display(z=z)
-
     if ren is None:
         ren = window.ren()
 
-    ren.add(vol_actor1)
-    ren.add(vol_actor2)
-    ren.add(vol_actor3)
+    shape = volume.shape
+    image_actor_z = actor.slicer(volume)
+    slicer_opacity = 0.6
+    image_actor_z.opacity(slicer_opacity)
 
-    return _inline_interact(ren, inline, interact)
+    image_actor_x = image_actor_z.copy()
+    x_midpoint = int(np.round(shape[0] / 2))
+    image_actor_x.display_extent(x_midpoint,
+                                x_midpoint, 0,
+                                shape[1] - 1,
+                                0,
+                                shape[2] - 1)
+
+    image_actor_y = image_actor_z.copy()
+    y_midpoint = int(np.round(shape[1] / 2))
+    image_actor_y.display_extent(0,
+                                shape[0] - 1,
+                                y_midpoint,
+                                y_midpoint,
+                                0,
+                                shape[2] - 1)
+
+    ren.add(image_actor_z)
+    ren.add(image_actor_x)
+    ren.add(image_actor_y)
+
+    show_m = window.ShowManager(ren, size=(1200, 900))
+    show_m.initialize()
+
+    line_slider_z = ui.LineSlider2D(min_value=0,
+                                    max_value=shape[2] - 1,
+                                    initial_value=shape[2] / 2,
+                                    text_template="{value:.0f}",
+                                    length=140)
+
+    line_slider_x = ui.LineSlider2D(min_value=0,
+                                    max_value=shape[0] - 1,
+                                    initial_value=shape[0] / 2,
+                                    text_template="{value:.0f}",
+                                    length=140)
+
+    line_slider_y = ui.LineSlider2D(min_value=0,
+                                    max_value=shape[1] - 1,
+                                    initial_value=shape[1] / 2,
+                                    text_template="{value:.0f}",
+                                    length=140)
+
+    opacity_slider = ui.LineSlider2D(min_value=0.0,
+                                    max_value=1.0,
+                                    initial_value=slicer_opacity,
+                                    length=140)
+
+    def change_slice_z(slider):
+        z = int(np.round(slider.value))
+        image_actor_z.display_extent(0, shape[0] - 1, 0, shape[1] - 1, z, z)
+
+    def change_slice_x(slider):
+        x = int(np.round(slider.value))
+        image_actor_x.display_extent(x, x, 0, shape[1] - 1, 0, shape[2] - 1)
+
+    def change_slice_y(slider):
+        y = int(np.round(slider.value))
+        image_actor_y.display_extent(0, shape[0] - 1, y, y, 0, shape[2] - 1)
+
+    def change_opacity(slider):
+        slicer_opacity = slider.value
+        image_actor_z.opacity(slicer_opacity)
+        image_actor_x.opacity(slicer_opacity)
+        image_actor_y.opacity(slicer_opacity)
+
+    line_slider_z.on_change = change_slice_z
+    line_slider_x.on_change = change_slice_x
+    line_slider_y.on_change = change_slice_y
+    opacity_slider.on_change = change_opacity
+
+    def build_label(text):
+        label = ui.TextBlock2D()
+        label.message = text
+        label.font_size = 18
+        label.font_family = 'Arial'
+        label.justification = 'left'
+        label.bold = False
+        label.italic = False
+        label.shadow = False
+        label.background = (0, 0, 0)
+        label.color = (1, 1, 1)
+
+        return label
+
+
+    line_slider_label_z = build_label(text="Z Slice")
+    line_slider_label_x = build_label(text="X Slice")
+    line_slider_label_y = build_label(text="Y Slice")
+    opacity_slider_label = build_label(text="Opacity")
+
+    panel = ui.Panel2D(size=(300, 200),
+                    color=(1, 1, 1),
+                    opacity=0.1,
+                    align="right")
+    panel.center = (1030, 120)
+
+    panel.add_element(line_slider_label_x, (0.1, 0.75))
+    panel.add_element(line_slider_x, (0.38, 0.75))
+    panel.add_element(line_slider_label_y, (0.1, 0.55))
+    panel.add_element(line_slider_y, (0.38, 0.55))
+    panel.add_element(line_slider_label_z, (0.1, 0.35))
+    panel.add_element(line_slider_z, (0.38, 0.35))
+    panel.add_element(opacity_slider_label, (0.1, 0.15))
+    panel.add_element(opacity_slider, (0.38, 0.15))
+
+    show_m.ren.add(panel)
+
+    global size
+    size = ren.GetSize()
+
+
+    def win_callback(obj, event):
+        global size
+        if size != obj.GetSize():
+            size_old = size
+            size = obj.GetSize()
+            size_change = [size[0] - size_old[0], 0]
+            panel.re_align(size_change)
+
+    show_m.initialize()
+
+    ren.zoom(1.5)
+    ren.reset_clipping_range()
+
+    if interact:
+        show_m.add_window_callback(win_callback)
+        show_m.render()
+        show_m.start()
+
+    return ren
