6627bf3634d64851e4df1783ebd8b9253c0533d2
Ariel Rokem <arokem@gmail.com>
2018-07-21 01:18:01 -0400

    Split streamlines that cross under some height.

--- AFQ/segmentation.py
+++ AFQ/segmentation.py
@@ -131,7 +131,7 @@ def gaussian_weights(bundle, n_points=100, return_mahalnobis=False):
     return w / np.sum(w, 0)
 
 
-def split_streamlines(streamlines, affine, low_coord=10):
+def split_streamlines(streamlines, template, low_coord=10):
     """
     Classify streamlines and split sl passing the mid-point below some height.
 
@@ -139,13 +139,12 @@ def split_streamlines(streamlines, affine, low_coord=10):
     ----------
     streamlines : list or Streamlines class instance.
 
-    affine : ndarray
+    template : nibabel.Nifti1Image class instance
         An affine transformation into a template space.
 
     low_coords: int
         How many coordinates below the 0,0,0 point should a streamline be to
         be split if it passes the midline.
-
 
     Returns
     -------
@@ -156,8 +155,38 @@ def split_streamlines(streamlines, affine, low_coord=10):
     below `low_coord` steps below the middle of the image (which should also
     be `low_coord` mms for templates with 1 mm resolution)
     """
-    # XXX Need to implement!
-    return streamlines, cross, left, lower_than
+    # Move the streamlines to the space of the template:
+    xform_sl = dts.Streamlines(dtu.move_streamlines(
+                    streamlines,
+                    np.linalg.inv(template.affine)))
+    # What is the x,y,z coordinate of 0,0,0 in the template space?
+    zero_coord = np.dot(np.linalg.inv(template.affine),
+                        np.array([0, 0, 0, 1]))
+    cross_below = zero_coord[2] - low_coord
+    # Initialize a boolean array that will contain a True if the array
+    # crosses the midline (even after splitting):
+    crosses = np.zeros(len(xform_sl), dtype=bool)
+    for sl_idx in range(len(xform_sl)):
+        sl = xform_sl[sl_idx]
+        if (np.any(sl[:, 0] > zero_coord[0]) and
+                np.any(sl[:, 0] < zero_coord[0])):
+            if np.any(sl[:, 2] < cross_below):
+                # This is a streamline that needs to be split where it
+                # crosses the midline:
+                split_idx = np.argmin(np.abs(sl[:, 0] - zero_coord[0]))
+                sl1 = sl[:split_idx]
+                sl2 = sl[split_idx:]
+                # Add them both in the end:
+                xform_sl = dts.Streamlines(list(xform_sl[:sl_idx]) +
+                                           list(xform_sl[sl_idx+1:]))
+                xform_sl.append(sl1)
+                xform_sl.append(sl2)
+                crosses = np.concatenate([crosses, np.zeros(2)])
+            else:
+                crosses[sl_idx] == True
+    # Move back to the original space:
+    streamlines = dtu.move_streamlines(xform_sl, template.affine)
+    return streamlines, crosses
 
 def _check_sl_with_inclusion(sl, include_rois, tol):
     """
@@ -241,15 +270,13 @@ def segment(fdata, fbval, fbvec, streamlines, bundle_dict, b0_threshold=0,
     # Classify the streamlines and split those that: 1) cross the
     # midline, and 2) pass under 10 mm below the mid-point of their
     # representation in the template space:
-    # streamlines, cross, lower_than, left, right = split_streamlines(
-    #                                                 streamlines,
-    #                                                 reg_template.affine)
+    sl_with_split, crosses = split_streamlines(streamlines, reg_template)
 
     if isinstance(mapping, str) or isinstance(mapping, nib.Nifti1Image):
         mapping = reg.read_mapping(mapping, img, reg_template)
 
     # Transform streamlines into the diffusion space:
-    xform_sl = dts.Streamlines(dtu.move_streamlines(streamlines,
+    xform_sl = dts.Streamlines(dtu.move_streamlines(sl_with_split,
                                                     np.linalg.inv(img.affine)))
 
     fiber_probabilities = np.zeros((len(xform_sl), len(bundle_dict)))
@@ -298,8 +325,7 @@ def segment(fdata, fbval, fbvec, streamlines, bundle_dict, b0_threshold=0,
         for sl_idx, sl in enumerate(xform_sl):
             if fiber_probabilities[sl_idx] > prob_threshold:
                 if crosses_midline is not None:
-                    if (np.any(sl[:, 0] > img.shape[0] // 2) and
-                            np.any(sl[:, 0] < img.shape[0] // 2)):
+                    if crosses[sl_idx]:
                         # This means that the streamline does
                         # cross the midline:
                         if crosses_midline:
