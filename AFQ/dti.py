67d35ab008a48c1fff458f1c3c7b822b091ebb7b
John K <jk23@att.net>
2019-07-01 13:20:07 -0700

    reduced memory usage of dti and added test for in place vector norm

--- AFQ/dti.py
+++ AFQ/dti.py
@@ -116,6 +116,64 @@ def predict(params_file, gtab, S0_file=None, out_dir=None):
 
     return fname
 
+def in_place_norm(vec, axis=-1, keepdims=False, delvec=True):
+    """ Return Vectors with Euclidean (L2) norm
+
+    See :term:`unit vector` and :term:`Euclidean norm`
+
+    Parameters
+    -------------
+    vec : array_like
+        Vectors to norm. Squared in the process of calculating the norm.
+    axis : int, optional
+        Axis over which to norm. By default norm over last axis. If `axis` is
+        None, `vec` is flattened then normed. Default is -1.
+    keepdims : bool, optional
+        If True, the output will have the same number of dimensions as `vec`,
+        with shape 1 on `axis`. Default is False.
+    delvec: bool, optional
+        If True, vec is deleted as soon as possible. Default is True. 
+        If False, vec is not deleted, but still squared.
+
+    Returns
+    ---------
+    norm : array
+        Euclidean norms of vectors.
+
+    Examples
+    --------
+    >>> vec = [[8, 15, 0], [0, 36, 77]]
+    >>> in_place_norm(vec)
+    array([ 17.,  85.])
+    >>> vec = [[8, 15, 0], [0, 36, 77]]
+    >>> in_place_norm(vec, keepdims=True)
+    array([[ 17.],
+           [ 85.]])
+    >>> vec = [[8, 15, 0], [0, 36, 77]]
+    >>> in_place_norm(vec, axis=0)
+    array([  8.,  39.,  77.])
+    """
+    vec = np.asarray(vec)
+    
+    if keepdims:
+        ndim = vec.ndim
+        shape = vec.shape
+    
+    vec *= vec
+    vec_norm = vec.sum(axis)
+    if delvec:
+        del vec
+    vec_norm **= 0.5
+    
+    if keepdims:
+        if axis is None:
+            shape = [1] * ndim
+        else:
+            shape = list(shape)
+            shape[axis] = 1
+        vec_norm = vec_norm.reshape(shape)
+
+    return vec_norm
 
 def tensor_odf(evals, evecs, sphere):
     """
@@ -130,13 +188,16 @@ def tensor_odf(evals, evecs, sphere):
     sphere : sphere object
         The ODF will be calculated in each vertex of this sphere.
     """
-    odf = np.zeros((evals.shape[:3] + (sphere.vertices.shape[0],)))
     mask = np.where((evals[..., 0] > 0)
                     & (evals[..., 1] > 0)
                     & (evals[..., 2] > 0))
 
-    lower = 4 * np.pi * np.sqrt(np.prod(evals[mask], -1))
     projection = np.dot(sphere.vertices, evecs[mask])
     projection /= np.sqrt(evals[mask])
-    odf[mask] = ((vector_norm(projection) ** -3) / lower).T
+    proj_norm = in_place_norm(projection)
+    proj_norm **= -3
+    proj_norm /= 4 * np.pi * np.sqrt(np.prod(evals[mask], -1))
+
+    odf = np.zeros((evals.shape[:3] + (sphere.vertices.shape[0],)))
+    odf[mask] = proj_norm.T
     return odf
