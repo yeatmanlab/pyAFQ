23eff550fa5746ba2475991a389dd0291ded6efb
John K <jk23@att.net>
2019-08-29 14:57:38 -0700

    removed gaussian weight test; done in dipy

--- AFQ/tests/test_segmentation.py
+++ AFQ/tests/test_segmentation.py
@@ -113,28 +113,3 @@ def test_segment():
     # This condition should still hold
     npt.assert_equal(len(fiber_groups), 2)
     npt.assert_(len(fiber_groups['CST_R']) > 0)
-
-def test_gaussian_weights():
-    # Some bogus x,y,z coordinates
-    x = np.arange(10)
-    y = np.arange(10)
-    z = np.arange(10)
-    # Create a distribution for which we can predict the weights we would
-    # expect to get:
-    bundle = np.array([np.array([x, y, z]).T + 1,
-                       np.array([x, y, z]).T - 1])
-    # In this case, all nodes receives an equal weight of 0.5:
-    w = seg.gaussian_weights(bundle)
-    npt.assert_equal(w, np.ones(bundle.shape[:-1]) * 0.5)
-
-    # Here, some nodes are twice as far from the mean as others
-    bundle = np.array([np.array([x, y, z]).T + 2,
-                       np.array([x, y, z]).T + 1,
-                       np.array([x, y, z]).T - 1,
-                       np.array([x, y, z]).T - 2])
-    w = seg.gaussian_weights(bundle)
-
-    # And their weights should be halved
-    # XXX Need to check how to transform this through the
-    # Gaussian distribution!
-    npt.assert_almost_equal(w[0], w[1] / 2)
