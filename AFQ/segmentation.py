690c71d717581a6394cdb70c17f3c51bda2ddc18
John K <jk23@att.net>
2019-10-15 14:50:34 -0700

    attemp pep8 fix

--- AFQ/segmentation.py
+++ AFQ/segmentation.py
@@ -222,10 +222,11 @@ class Segmentation:
         Quantification" PloS One 7 (11): e49790.
         """
         if img_affine is not None:
-            if mapping is None or \
-                    fdata is not None or \
-                fbval is not None or \
-                    fbvec is not None:
+            if (mapping is None or
+                fdata is not None or
+                fbval is not None or
+                    fbvec is not None):
+
                 self.logger.error(
                     "Provide either the full path to data, bvals, bvecs," +
                     "or provide the affine of the image and the mapping")
