eb2b1e9f765084bf8eb10cc03573a03c9f90c3a0
John K <jk23@att.net>
2019-10-22 14:06:38 -0700

    added two tqdm in segmentation

--- AFQ/segmentation.py
+++ AFQ/segmentation.py
@@ -472,7 +472,8 @@ class Segmentation:
 
         self.logger.info("Assigning Streamlines to Bundles...")
         tol = dts.dist_to_corner(self.img_affine)**2
-        for bundle_idx, bundle in enumerate(self.bundle_dict):
+
+        for bundle_idx, bundle in enumerate(tqdm(self.bundle_dict)):
             self.logger.info(f"Finding Streamlines for {bundle}")
             warped_prob_map, include_roi, exclude_roi = \
                 self._get_bundle_info(bundle_idx, bundle)
@@ -532,7 +533,7 @@ class Segmentation:
         # streamlines within a bundle in the same orientation with respect to
         # the ROIs. This order is ARBITRARY but CONSISTENT (going from ROI0
         # to ROI1).
-        for bundle_idx, bundle in enumerate(self.bundle_dict):
+        for bundle_idx, bundle in enumerate(tqdm(self.bundle_dict)):
             select_idx = np.where(bundle_choice == bundle_idx)
             # Use a list here, Streamlines don't support item assignment:
             select_sl = list(streamlines[select_idx])
