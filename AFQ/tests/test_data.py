f4723c07af3869433436ba1d4110a37f3df0230f
Ariel Rokem <arokem@gmail.com>
2019-12-05 13:36:30 -0800

    Transform bundle names into indices.

--- AFQ/tests/test_data.py
+++ AFQ/tests/test_data.py
@@ -6,7 +6,6 @@ import AFQ.data as afd
 
 
 def test_aal_to_regions():
-
     atlas = np.zeros((20, 20, 20, 5))
     # Just one region:
     atlas[0, 0, 0, 0] = 1
@@ -34,3 +33,20 @@ def test_aal_to_regions():
                           | (atlas[..., 0] == 2)
                           | (atlas[..., 1] == 1))).T)
 
+
+def test_bundles_to_aal():
+    atlas = np.zeros((20, 20, 20, 5))
+
+    atlas[0, 0, 0, 0] = 1
+
+    targets = afd.bundles_to_aal(["ATR_L"], atlas)
+    npt.assert_equal(targets,
+                     [[None, np.array(np.where(atlas[..., 0] == 1)).T]])
+
+
+    atlas[0, 0, 1, 0] = 2
+
+    targets = afd.bundles_to_aal(["ATR_L", "ATR_R"], atlas)
+    npt.assert_equal(targets,
+                     [[None, np.array(np.where(atlas[..., 0] == 1)).T],
+                      [None, np.array(np.where(atlas[..., 0] == 2)).T]])
