07fda65a330a3631ab5465798e7b6d86ffad3835
Ariel Rokem <arokem@gmail.com>
2019-12-10 10:40:24 -0800

    Removes `write_trk` function with obsolete API.
    We should use nib/DIPY i/o functions directly instead.

--- AFQ/utils/streamlines.py
+++ AFQ/utils/streamlines.py
@@ -1,43 +1,5 @@
 import numpy as np
 import nibabel as nib
-from nibabel import trackvis
-import dipy.tracking.utils as dtu
-
-
-def write_trk(fname, streamlines, affine=None, shape=None):
-    """
-    Write out a .trk file
-
-    Parameters
-    ----------
-    fname : str
-        Full path to save the file into
-    streamlines : list
-        A list of arrays of 3D coordinates
-    affine : array (4,4), optional.
-        An affine transformation associated with the streamlines. Defaults to
-        identity.
-    shape : 3-element tuple, optional
-        Spatial dimensions of an image associated with the streamlines.
-        Defaults to not be set in the file header.
-    """
-    if affine is None:
-        affine = np.eye(4)
-
-    zooms = np.sqrt((affine * affine).sum(0))
-    streamlines = dtu.move_streamlines(streamlines, affine)
-    data = ((s, None, None) for s in streamlines)
-
-    voxel_order = nib.orientations.aff2axcodes(affine)
-    voxel_order = "".join(voxel_order)
-
-    hdr = nib.trackvis.empty_header()
-    hdr['voxel_size'] = zooms[:3]
-    hdr['voxel_order'] = voxel_order
-    hdr['vox_to_ras'] = affine
-    if shape is not None:
-        hdr['dim'] = shape
-    trackvis.write(fname, data, hdr, points_space="rasmm")
 
 
 def add_bundles(t1, t2):
