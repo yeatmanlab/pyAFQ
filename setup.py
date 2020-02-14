35b160cb984a0ed85d48e1c07100d478233490b3
Ariel Rokem <arokem@gmail.com>
2020-02-14 12:41:24 -0800

    Trying to deal with version string again.

--- setup.py
+++ setup.py
@@ -29,13 +29,16 @@ def local_version(version):
     """
     Patch in a version that can be uploaded to test PyPI
     """
-    gh_in_int = []
-    for char in version.node:
-        if char.isdigit():
-            gh_in_int.append(str(char))
-        else:
-            gh_in_int.append(str(string.ascii_letters.find(char)))
-    return "".join(gh_in_int)
+    # print(version.__dict__)
+    # print(version.tag.__dict__)
+    # gh_in_int = []
+    # for char in version.node:
+    #     if char.isdigit():
+    #         gh_in_int.append(str(char))
+    #     else:
+    #         gh_in_int.append(str(string.ascii_letters.find(char)))
+    #    return "".join(gh_in_int)
+    return str(version.distance)
 
 opts = dict(name=NAME,
             maintainer=MAINTAINER,
