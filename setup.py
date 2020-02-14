d6b2ef75ecf0674d5648e543221c16452646a662
Ariel Rokem <arokem@gmail.com>
2020-02-14 12:47:22 -0800

    Checks the version string for indication that this is a dev version.

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import find_packages
 import string
 import os.path as op
+from setuptools_scm import get_version
 
 try:
     from setuptools import setup
@@ -29,16 +30,18 @@ def local_version(version):
     """
     Patch in a version that can be uploaded to test PyPI
     """
-    # print(version.__dict__)
-    # print(version.tag.__dict__)
-    # gh_in_int = []
-    # for char in version.node:
-    #     if char.isdigit():
-    #         gh_in_int.append(str(char))
-    #     else:
-    #         gh_in_int.append(str(string.ascii_letters.find(char)))
-    #    return "".join(gh_in_int)
-    return str(version.distance)
+    scm_version = get_version()
+    if "dev" in scm_version:
+        gh_in_int = []
+        for char in version.node:
+            if char.isdigit():
+                gh_in_int.append(str(char))
+            else:
+                gh_in_int.append(str(string.ascii_letters.find(char)))
+        return "".join(gh_in_int)
+    else:
+        return ""
+
 
 opts = dict(name=NAME,
             maintainer=MAINTAINER,
