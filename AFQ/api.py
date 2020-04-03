b9f082114a13b7b7418c48058757ede0a68c9ea9
John K <jk23@att.net>
2020-04-03 12:15:57 -0700

    remove print lines, added tests

--- AFQ/api.py
+++ AFQ/api.py
@@ -324,10 +324,6 @@ class AFQ(object):
         for subject, sub_dir in zip(self.subjects, self.dmriprep_dirs):
             sessions = glob.glob(op.join(sub_dir, '*'))
             for sess in sessions:
-                print(f"{sess}/{dwi_folder}/{dwi_file}.nii.gz")
-                print(glob.glob(f"{sess}/{dwi_folder}/{dwi_file}.nii.gz"))
-                print(glob.glob(f"{sess}/{anat_folder}/{anat_file}.nii.gz"))
-                print(glob.glob(f"{sess}/{anat_folder}/{seg_file}.nii.gz"))
                 results_dir_list.append(op.join(self.afq_dir,
                                                 subject,
                                                 PurePath(sess).parts[-1]))
