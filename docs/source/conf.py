9a22b8be01c99394e52c07b2b99aa544fb5bef83
Ariel Rokem <arokem@gmail.com>
2020-04-03 13:35:54 -0700

    Update the links to intersphinx inventory locations.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -198,9 +198,9 @@ texinfo_documents = [
 # -- Options for intersphinx extension ---------------------------------------
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'python': ('https://docs.python.org/', None),
-                       'numpy': ('http://http://docs.scipy.org/doc/numpy/', None),
-                       'dipy': ('http://docs.dipy.org', None)
+intersphinx_mapping = {'python': ('https://docs.python.org/3/', None),
+                       'numpy': ('https://docs.scipy.org/doc/numpy/', None),
+                       'dipy': ('https://dipy.org/documentation/latest/', None)
                         }
 
 image_scrapers = ('matplotlib', plotly_sg_scraper,)
