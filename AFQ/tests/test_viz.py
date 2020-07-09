3c217d186376ad9ac9bba143e59c4f31cd08390d
Ariel Rokem <arokem@gmail.com>
2020-07-08 19:51:16 -0700

    Adapt viz test to new API.

--- AFQ/tests/test_viz.py
+++ AFQ/tests/test_viz.py
@@ -5,11 +5,11 @@ import nibabel.tmpdirs as nbtmp
 import AFQ.data as afd
 from AFQ import api
 
-
 def test_plot_tract_profiles():
     tmpdir = nbtmp.InTemporaryDirectory()
     afd.organize_stanford_data(path=tmpdir.name)
-    myafq = api.AFQ(dmriprep_path=op.join(tmpdir.name, 'stanford_hardi',
-                                          'derivatives', 'dmriprep'),
-                    sub_prefix='sub')
+    bids_path=op.join(tmpdir.name, 'stanford_hardi')
+    myafq = api.AFQ(bids_path=bids_path,
+                dmriprep='vistasoft',
+                segmentation='freesurfer')
     myafq.plot_tract_profiles()
