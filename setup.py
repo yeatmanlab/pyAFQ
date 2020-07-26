0725d7c305fecf5f6322231a97d873e83ee41e74
Ariel Rokem <arokem@gmail.com>
2020-07-26 15:58:20 -0700

    We might still need this hack.

--- setup.py
+++ setup.py
@@ -1,5 +1,23 @@
 from setuptools import setup
+import string
+from setuptools_scm import get_version
 
+
+def local_version(version):
+    """
+    Patch in a version that can be uploaded to test PyPI
+    """
+    scm_version = get_version()
+    if "dev" in scm_version:
+        gh_in_int = []
+        for char in version.node:
+            if char.isdigit():
+                gh_in_int.append(str(char))
+            else:
+                gh_in_int.append(str(string.ascii_letters.find(char)))
+        return "".join(gh_in_int)
+    else:
+        return ""
 
 opts = dict(use_scm_version={"root": ".", "relative_to": __file__,
                              "write_to": "AFQ/version.py",
