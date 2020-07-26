da55b12e8ee32466ac65c0e2fcdd40a4df620b21
Ariel Rokem <arokem@gmail.com>
2020-07-26 15:56:16 -0700

    MAINT: Adds setup.cfg, instead of the meta file and a lot of setup.py cruft.
    Hopefully addresses https://github.com/yeatmanlab/pyAFQ/issues/306, and also
    overall reduces the amount of hackiness.

--- setup.py
+++ setup.py
@@ -1,81 +1,9 @@
-from setuptools import find_packages
-import string
-import os.path as op
-from setuptools_scm import get_version
-import glob
-
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-
-here = op.abspath(op.dirname(__file__))
-
-# Get metadata from the AFQ/version.py file:
-meta_file = op.join(here, 'AFQ', '_meta.py')
-with open(meta_file) as f:
-    exec(f.read())
-
-REQUIRES = []
-with open(op.join(here, 'requirements.txt')) as f:
-    ll = f.readline()[:-1]
-    while ll:
-        REQUIRES.append(ll)
-        ll = f.readline()[:-1]
-
-EXTRAS_REQUIRE = {}
-for extra_req_file in glob.glob(op.join(here, 'requirements-*.txt')):
-    extra_name = extra_req_file.split('-')[1].split('.')[0]
-    extra_reqs = []
-    with open(extra_req_file) as f:
-        ll = f.readline()[:-1]
-        while ll:
-            extra_reqs.append(ll)
-            ll = f.readline()[:-1]
-    EXTRAS_REQUIRE[extra_name] = extra_reqs
-
-with open(op.join(here, 'README.md'), encoding='utf-8') as f:
-    LONG_DESCRIPTION = f.read()
+from setuptools import setup
 
 
-def local_version(version):
-    """
-    Patch in a version that can be uploaded to test PyPI
-    """
-    scm_version = get_version()
-    if "dev" in scm_version:
-        gh_in_int = []
-        for char in version.node:
-            if char.isdigit():
-                gh_in_int.append(str(char))
-            else:
-                gh_in_int.append(str(string.ascii_letters.find(char)))
-        return "".join(gh_in_int)
-    else:
-        return ""
-
-
-opts = dict(name=NAME,
-            maintainer=MAINTAINER,
-            maintainer_email=MAINTAINER_EMAIL,
-            description=DESCRIPTION,
-            long_description=LONG_DESCRIPTION,
-            url=URL,
-            download_url=DOWNLOAD_URL,
-            license=LICENSE,
-            classifiers=CLASSIFIERS,
-            author=AUTHOR,
-            author_email=AUTHOR_EMAIL,
-            platforms=PLATFORMS,
-            packages=find_packages(),
-            install_requires=REQUIRES,
-            extras_require=EXTRAS_REQUIRE,
-            scripts=SCRIPTS,
-            python_requires=PYTHON_REQUIRES,
-            use_scm_version={"root": ".", "relative_to": __file__,
+opts = dict(use_scm_version={"root": ".", "relative_to": __file__,
                              "write_to": "AFQ/version.py",
-                             "local_scheme": local_version},
-            setup_requires=['setuptools_scm'])
+                             "local_scheme": local_version})
 
 
 if __name__ == '__main__':
