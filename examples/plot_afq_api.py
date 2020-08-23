647f70604fdbbdb73f1e526f3857ab1e11b84c94
Ariel Rokem <arokem@gmail.com>
2020-08-22 19:24:44 -0700

    Update the API example to refer to the right df column,
    Also, we might want to always run this in the CI. Let's see how
    long this takes.

--- examples/plot_afq_api.py
+++ examples/plot_afq_api.py
@@ -61,22 +61,14 @@ ax.axis("off")
 ##########################################################################
 # Getting tract profiles:
 # -------------------------
-# The computation below is quite time-consuming. For this reason, we have
-# commented it out for now. If you want to run it through, uncomment then next
-# few lines and go get a cup of coffee:
-#
-# .. note::
-#     Note that because of a quirk in the way that brain segmentation was
-#     generated in this data, some bundles will not be detected using the
-#     detected using the default values. For example, the corticospinal tracts
-#     on both sides will not be properly detected because the waypoint ROIs for
-#     these tracts fall into parts of the white matter that were not segmented.
+# Running the code below triggers the full pipeline of operations leading
+# to the computation of the tract profiles. Therefore, it takes a little
+# while to run (about 40 minutes in a recent experiment run on a laptop).
 
-# df = pd.read_csv(myafq.tract_profiles[0])
-# for bundle in df['bundle'].unique():
-#     fig, ax = plt.subplots(1)
-#     ax.plot(df[(df['scalar'] == "dti_fa")
-#             & (df['bundle'] == bundle)]['profiles'])
-#     ax.set_title(bundle)
+df = pd.read_csv(myafq.tract_profiles[0])
+for bundle in df['bundle'].unique():
+    fig, ax = plt.subplots(1)
+    ax.plot(df[(df['bundle'] == bundle)]['dti_fa'])
+    ax.set_title(bundle)
 
 plt.show()
