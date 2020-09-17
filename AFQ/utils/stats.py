10f4c7ebdd81b8b1280c9716fdb8321bd37fe9a9
John K <jk23@att.net>
2020-09-17 10:18:17 -0700

    double the ci

--- AFQ/utils/stats.py
+++ AFQ/utils/stats.py
@@ -1,4 +1,4 @@
-def contrast_index(x1, x2):
+def contrast_index(x1, x2, double=True):
     """
     Calculate the contrast index between two arrays.
     The contrast index is symmetrical with respect to
@@ -12,10 +12,18 @@ def contrast_index(x1, x2):
     x2 : ndarray of floats
         An ndarray to compare. The contrast index will have negative values
         where x2 > x1.
+    double : bool, optional
+        Double the contrast index.
+        This makes it comparable to a percent difference.
+        Default: True
 
     Returns
     -------
     contrast_index : ndarray of floats
         Contrast index calculated by doing (x1 - x2) / (x1 + x2)
     """
-    return (x1 - x2) / (x1 + x2)
+    ci = (x1 - x2) / (x1 + x2)
+    if double:
+        return ci * 2
+    else:
+        return ci
