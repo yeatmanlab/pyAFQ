533351564b4f3e57364b9c129d9d63ed152fcd51
John K <jk23@att.net>
2020-09-24 16:06:41 -0700

    add tests

--- AFQ/tests/test_api.py
+++ AFQ/tests/test_api.py
@@ -277,6 +277,39 @@ def test_AFQ_anisotropic():
         'sub-01_ses-01_dwi_model-CSD_APM.nii.gz'))
 
 
+def test_API_type_checking():
+    _, bids_path, _ = get_temp_hardi()
+    with pytest.raises(
+        TypeError,
+        match="bids_path must be a string"):
+        api.AFQ(2)
+
+    with pytest.raises(
+        TypeError,
+        match="custom_tractography_bids_filters must be"
+              + " either a dict or None"):
+        api.AFQ(
+            bids_path,
+            custom_tractography_bids_filters=["dwi"])
+
+    with pytest.raises(
+        TypeError,
+        match="brain_mask must be None or a mask defined in `AFQ.mask`"):
+        api.AFQ(
+            bids_path,
+            brain_mask="not a brain mask")
+
+    with pytest.raises(
+        TypeError,
+        match="viz_backend must contain either 'fury' or 'plotly'"):
+        api.AFQ(bids_path, viz_backend="matplotlib")
+
+    with pytest.raises(
+        TypeError,
+        match="bundle_names must be None or a list of strings"):
+        api.AFQ(bids_path, bundle_names=[2, 3])
+
+
 @pytest.mark.skip(reason="may cause OOM")
 def test_AFQ_slr():
     """
