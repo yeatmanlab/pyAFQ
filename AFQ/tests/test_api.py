1d0690a5dbf876d1e06daa071745a389800e6ff3
John K <jk23@att.net>
2020-10-07 10:51:01 -0700

    add reco80 test

--- AFQ/tests/test_api.py
+++ AFQ/tests/test_api.py
@@ -344,6 +344,23 @@ def test_AFQ_reco():
     myafq.export_all()
 
 
+def test_AFQ_reco80():
+    """
+    Test if API can run registeration with FA
+    """
+    _, bids_path, _ = get_temp_hardi()
+    myafq = api.AFQ(
+        bids_path=bids_path,
+        dmriprep='vistasoft',
+        segmentation_params={
+            'seg_algo': 'reco80',
+            'rng': 42})
+
+    tgram = load_tractogram(myafq.get_clean_bundles()[0], myafq.dwi_img[0])
+    bundles = aus.tgram_to_bundles(tgram, myafq.bundle_dict, myafq.dwi_img[0])
+    npt.assert_(len(bundles['CCMid']) > 0)
+
+
 @pytest.mark.nightly2
 def test_AFQ_pft():
     """
