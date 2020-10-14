f959c5f7ef482e2b094b5e6ff079475d2b7869c1
Ariel Rokem <arokem@gmail.com>
2020-10-13 20:53:32 -0700

    PEP8 fixes.

--- AFQ/tests/test_data.py
+++ AFQ/tests/test_data.py
@@ -212,11 +212,12 @@ def test_bundles_to_aal():
         ],
     )
 
+
 def test_read_roi():
     aff1 = np.eye(4)
     template = nib.Nifti1Image(np.ones((10, 10, 10)), aff1)
     aff2 = aff1[:]
-    aff2[0,0] = -1
+    aff2[0, 0] = -1
     roi = nib.Nifti1Image(np.zeros((10, 10, 10)), aff2)
     img = afd.read_roi(roi, resample_to=template)
     npt.assert_equal(img.affine, template.affine)
