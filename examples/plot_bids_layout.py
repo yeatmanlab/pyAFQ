8de3d7674eff7861b9283f1486c3cbce62a2f2f0
Ariel Rokem <arokem@gmail.com>
2020-11-02 15:39:10 -0800

    Starting to overhaul example to be more generally about BIDS.

--- examples/plot_bids_layout.py
+++ examples/plot_bids_layout.py
@@ -1,42 +1,85 @@
 """
-=============================================
-Segmenting tractography from another pipeline
-=============================================
+====================
+How pyAFQ uses BIDS
+====================
 
-The AFQ API provides facilities to segment tractography results obtained using
-other software. For example, we often use
-`qsiprep <https://qsiprep.readthedocs.io/en/latest/>`_ to preprocess our data
-and reconstruct tractographies with software such as
-`MRTRIX <https://www.mrtrix.org/>`_. Here, we will demonstrate how to use
-these reconstructions in the pyAFQ segmentation and tractometry pipeline.
+The pyAFQ API relies heavily on the
+`Brain Imaging Data Standard (BIDS) <https://bids-specification.readthedocs.io/en/stable/>`_. This means that the software assumes that its inputs are organized
+according to the BIDS spec and its outputs conform where possible with the
+BIDS spec.
+
+.. note::
+
+    Derivatives of processing diffusion MRI are not currently fully
+    described in the existing BIDS specification, but describing these
+    is part of an ongoing effort. Wherever possible, we conform with
+    the draft implementation of the BIDS DWI derivatives available
+    `here <https://bids-specification.readthedocs.io/en/derivatives/05-derivatives/06-diffusion-derivatives.html>`_
+
+In this example, we will explore the use of BIDS in pyAFQ and see
+how BIDS allows us to extend and provide flexibility to the users
+of the software.
 
 """
+
 import os
 import os.path as op
 
 import matplotlib.pyplot as plt
 import nibabel as nib
-import plotly
 
 from AFQ import api
 import AFQ.data as afd
 
+import bids
+
+
 ##########################################################################
-# Example data
-# ---------------------
-# The example data we will use here is generated from the Stanford HARDI
-# dataset (https://purl.stanford.edu/ng782rw8378). The calls below fetch
-# the results of tractography with this dataset and organize it within
-# the `~/AFQ_data` folder.
+# We start with some example data. The data we will use here is
+# generated from the Stanford HARDI dataset
+# (https://purl.stanford.edu/ng782rw8378). The call below fetches
+# this dataset and organized it within the `~/AFQ_data` folder in the BIDS
+# format.
 
 afd.organize_stanford_data()
-afd.fetch_stanford_hardi_tractography()
 
 ##########################################################################
-# Reorganize data
-# ---------------------
-# We organize the data so that it conforms with the BIDS standard for
-# derivatives:
+# After doing that, we should have a folder that looks like this:
+#
+# | stanford_hardi
+# | ├── dataset_description.json
+# | └── derivatives
+# |     ├── freesurfer
+# |     │   ├── dataset_description.json
+# |     │   └── sub-01
+# |     │       └── ses-01
+# |     │           └── anat
+# |     │               ├── sub-01_ses-01_T1w.nii.gz
+# |     │               └── sub-01_ses-01_seg.nii.gz
+# |     └── vistasoft
+# |         ├── dataset_description.json
+# |         └── sub-01
+# |             └── ses-01
+# |                 └── dwi
+# |                     ├── sub-01_ses-01_dwi.bvals
+# |                     ├── sub-01_ses-01_dwi.bvecs
+# |                     └── sub-01_ses-01_dwi.nii.gz
+#
+# The top level directory is our overall bids dataset folder. In most
+# cases, this folder will include a `raw` folder that will contain the
+# raw data. In this case, we do not include the raw folder and only have
+# the derivatives folder that contains the outputs of preprocessing the data.
+# In this case, one folder containing Freesurfer derivatives and another
+# folder containing the DWI data that has been preprocessed with Vistasoft.
+# pyAFQ provides facilities to segment tractography results obtained
+# using other software. For example, we often use
+# `qsiprep <https://qsiprep.readthedocs.io/en/latest/>`_ to preprocess
+# our data and reconstruct tractographies with software such as
+# `MRTRIX <https://www.mrtrix.org/>`_. Here, we will demonstrate how to use
+# these reconstructions in the pyAFQ segmentation and tractometry pipeline
+# We fetch this data and add it as a separate derivatives folder
+
+afd.fetch_stanford_hardi_tractography()
 
 bids_path = op.join(op.expanduser('~'), 'AFQ_data', 'stanford_hardi')
 tractography_path = op.join(bids_path, 'derivatives', 'my_tractography')
@@ -59,9 +102,8 @@ afd.to_bids_description(
         "PipelineDescription": {"Name": "my_tractography"}})
 
 
-##########################################################################
-# Once this is done, you should have a folder in your home directory that
-# looks like this:
+###########################################################################
+# After we do that, our dataset folder should look like this:
 #
 # | stanford_hardi
 # | ├── dataset_description.json
@@ -87,6 +129,9 @@ afd.to_bids_description(
 # |                     ├── sub-01_ses-01_dwi.bvals
 # |                     ├── sub-01_ses-01_dwi.bvecs
 # |                     └── sub-01_ses-01_dwi.nii.gz
+#
+# To explore the layout of these derivatives, we will initialize a
+# :class:`BIDSLayout` class instance to help us see what is in this dataset
 
 
 ##########################################################################
@@ -132,3 +177,10 @@ my_afq = api.AFQ(
 # AFQ within the 'stanford_hardi/derivatives/afq' folder.
 
 my_afq.export_all()
+
+
+##########################################################################
+# A few common issues that can hinder BIDS from working properly are:
+#
+# 1. Faulty dataset_description.json
+# 2. File naming convention doesn't uniquely identify file with bids filters
