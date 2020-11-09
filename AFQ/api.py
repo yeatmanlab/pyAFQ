8bbbde43e66efd1e795df44b0cb50ed43700cc1b
Ariel Rokem <arokem@gmail.com>
2020-11-08 18:57:51 -0800

    Load the CSD sh coefficients from file.

--- AFQ/api.py
+++ AFQ/api.py
@@ -747,7 +747,7 @@ class AFQ(object):
             row, '_model-CSD_APM.nii.gz')
         if not op.exists(pmap_file):
             dwi_data, gtab, img = self._get_data_gtab(row)
-            sh_coeff = self._csd(row)
+            sh_coeff = nib.load(self._csd(row)).get_fdata()
             pmap = shm.anisotropic_power(sh_coeff)
             pmap = nib.Nifti1Image(pmap, img.affine)
             self.log_and_save_nii(pmap, pmap_file)
