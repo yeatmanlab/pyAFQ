1190b23f016b58708c7db65d18157455adedf0dc
John K <jk23@att.net>
2021-02-07 16:30:08 -0800

    fix serialization error

--- AFQ/api.py
+++ AFQ/api.py
@@ -1305,7 +1305,6 @@ class AFQ(object):
             row['timing']['Segmentation'] =\
                 row['timing']['Segmentation'] + time() - start_time
             for bundle in self.bundle_dict.keys():
-                print(bundles)
                 if bundle != "whole_brain":
                     row["sl_counts"].at[bundle, "n_streamlines"] =\
                         len(bundles[bundle].streamlines)
