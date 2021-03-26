54188e082f7a6bbd847768d3a2a4a1d004be547d
John K <jk23@att.net>
2021-03-26 15:22:00 -0700

    give full path

--- afqsi_docker/docker-build.sh
+++ afqsi_docker/docker-build.sh
@@ -3,5 +3,5 @@ export COMMIT
 TAG="${2}/afqsi:${COMMIT}"
 TAG="$(echo "${TAG}" | tr -d '[:space:]')"
 echo $TAG
-docker build --no-cache -t $TAG .
+docker build --no-cache -t $TAG ./afqsi_docker
 docker push $TAG
--- pyafq_docker/docker-build.sh
+++ pyafq_docker/docker-build.sh
@@ -3,5 +3,5 @@ export COMMIT
 TAG="${2}/pyafq:${COMMIT}"
 TAG="$(echo "${TAG}" | tr -d '[:space:]')"
 echo $TAG
-docker build --no-cache -t $TAG .
+docker build --no-cache -t $TAG ./pyafq_docker
 docker push $TAG
