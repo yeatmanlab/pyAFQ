05b3e471de532b8c5ee98017cd77c13ca6646ce4
John K <jk23@att.net>
2021-03-26 15:06:25 -0700

    hopefully last bash syntax error

--- afqsi_docker/docker-build.sh
+++ afqsi_docker/docker-build.sh
@@ -1,7 +1,7 @@
 COMMIT=${1}
 export COMMIT
 TAG="${2}/afqsi:${COMMIT}"
-TAG="$(echo -e "${TAG}" | tr -d '[:space:]')"
+TAG="$(echo "${TAG}" | tr -d '[:space:]')"
 echo $TAG
 docker build --no-cache -t $TAG .
 docker push $TAG
