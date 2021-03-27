ff96ec21425fc746464975ec72757ae0d88554dc
John K <jk23@att.net>
2021-03-26 19:14:07 -0700

    this should be in tag?

--- afqsi_docker/docker-build.sh
+++ afqsi_docker/docker-build.sh
@@ -1,8 +1,8 @@
 COMMIT=${1}
 COMMIT="$(echo "${COMMIT}" | tr -d '[:space:]')"
 export COMMIT
-TAG="${2}/afqsi:${COMMIT}"
+TAG="ghcr.io/${2}/afqsi:${COMMIT}"
 TAG="$(echo "${TAG}" | tr -d '[:space:]')"
 echo $TAG
 docker build --no-cache -t $TAG --build-arg COMMIT ./afqsi_docker
-docker push ghcr.io/$TAG
+docker push $TAG
--- pyafq_docker/docker-build.sh
+++ pyafq_docker/docker-build.sh
@@ -1,8 +1,8 @@
 COMMIT=${1}
 COMMIT="$(echo "${COMMIT}" | tr -d '[:space:]')"
 export COMMIT
-TAG="${2}/pyafq:${COMMIT}"
+TAG="ghcr.io/${2}/pyafq:${COMMIT}"
 TAG="$(echo "${TAG}" | tr -d '[:space:]')"
 echo $TAG
 docker build --no-cache -t $TAG --build-arg COMMIT ./pyafq_docker
-docker push ghcr.io/$TAG
+docker push $TAG
