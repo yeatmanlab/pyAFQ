821712894294900b65fa0d579114248d09474556
John K <jk23@att.net>
2021-03-26 18:58:01 -0700

    add build arg

--- afqsi_docker/docker-build.sh
+++ afqsi_docker/docker-build.sh
@@ -1,7 +1,8 @@
 COMMIT=${1}
+COMMIT="$(echo "${COMMIT}" | tr -d '[:space:]')"
 export COMMIT
 TAG="${2}/afqsi:${COMMIT}"
 TAG="$(echo "${TAG}" | tr -d '[:space:]')"
 echo $TAG
-docker build --no-cache -t $TAG ./afqsi_docker
+docker build --no-cache -t $TAG --build-arg COMMIT ./afqsi_docker
 docker push $TAG
--- pyafq_docker/docker-build.sh
+++ pyafq_docker/docker-build.sh
@@ -1,7 +1,8 @@
 COMMIT=${1}
+COMMIT="$(echo "${COMMIT}" | tr -d '[:space:]')"
 export COMMIT
 TAG="${2}/pyafq:${COMMIT}"
 TAG="$(echo "${TAG}" | tr -d '[:space:]')"
 echo $TAG
-docker build --no-cache -t $TAG ./pyafq_docker
+docker build --no-cache -t $TAG --build-arg COMMIT ./pyafq_docker
 docker push $TAG
