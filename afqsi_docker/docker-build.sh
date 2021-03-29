0dd21305d8eeaaa485baa450e7745cfd5ed1c507
John K <jk23@att.net>
2021-03-29 10:48:46 -0700

    also test qsiafq

--- afqsi_docker/docker-build.sh
+++ afqsi_docker/docker-build.sh
@@ -5,4 +5,5 @@ TAG="ghcr.io/${2}/afqsi:${COMMIT}"
 TAG="$(echo "${TAG}" | tr -d '[:space:]')"
 echo $TAG
 docker build --no-cache -t $TAG --build-arg COMMIT ./afqsi_docker
+docker run --rm -it $TAG pytest /usr/local/lib/python3.7/site-packages/AFQ/tests/test_api.py::test_AFQ_data_waypoint
 docker push $TAG
