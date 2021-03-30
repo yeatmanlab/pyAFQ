bdea63200b6b5d21710a954f21ce2f51690e5f24
John K <jk23@att.net>
2021-03-30 10:01:28 -0700

    properly format no tag

--- pyafq_docker/docker-build.sh
+++ pyafq_docker/docker-build.sh
@@ -1,9 +1,13 @@
 COMMIT=${1}
 COMMIT="$(echo "${COMMIT}" | tr -d '[:space:]')"
 export COMMIT
-TAG="ghcr.io/${2}/pyafq:${COMMIT}"
-TAG2="ghcr.io/${2}/pyafq:latest"
+NO_TAG="ghcr.io/${2}/pyafq"
+TAG="${NO_TAG}:${COMMIT}"
+TAG2="${NO_TAG}:latest"
 TAG="$(echo "${TAG}" | tr -d '[:space:]')"
+TAG2="$(echo "${TAG2}" | tr -d '[:space:]')"
+NO_TAG="$(echo "${NO_TAG}" | tr -d '[:space:]')"
+
 echo $TAG
 docker build --no-cache -t $TAG -t $TAG2 --build-arg COMMIT ./pyafq_docker
-docker push --all-tags ghcr.io/${2}/pyafq
+docker push --all-tags $NO_TAG
