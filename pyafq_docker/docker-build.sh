6c4f2446f62821f37c6d01b9a8e12b90906ac0ce
John K <jk23@att.net>
2021-11-10 14:59:55 -0800

    fix tag bug

--- pyafq_docker/docker-build.sh
+++ pyafq_docker/docker-build.sh
@@ -1,6 +1,7 @@
 COMMIT=${1}
 COMMIT="$(echo "${COMMIT}" | tr -d '[:space:]')"
 export COMMIT
+NO_TAG="ghcr.io/${2}/pyafq"
 TAG="${NO_TAG}:${COMMIT}"
 TAG2="${NO_TAG}:latest"
 TAG="$(echo "${TAG}" | tr -d '[:space:]')"
