cbfa255f4b47f87258d767881bfa57f03dc9aaf8
36000 <jk6.28@outlook.com>
2022-05-11 13:22:53 -0700

    update for windows compat; BF

--- AFQ/utils/path.py
+++ AFQ/utils/path.py
@@ -1,4 +1,6 @@
+import os.path as op
+
+
 def drop_extension(path):
-    base_fname = path.split('/')[-1].split('.')[0]
-    path.split(base_fname)[0] + base_fname
-    return path
+    base_fname = op.basename(path).split('.')[0]
+    return path.split(base_fname)[0] + base_fname
