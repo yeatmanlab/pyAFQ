224cf40c9fd7334783da20065ce8ea17d327de4a
36000 <jk6.28@outlook.com>
2022-05-11 13:32:00 -0700

    pep8 slash escapes

--- AFQ/utils/tests/test_path.py
+++ AFQ/utils/tests/test_path.py
@@ -4,6 +4,6 @@ from AFQ.utils.path import drop_extension
 def test_drop_extension():
     assert "/my/.example/.path/to_file" == drop_extension(
         "/my/.example/.path/to_file.nii.gz")
-    
-    assert "C:\Documents\Newsletters\Summer2018" == drop_extension(
-        "C:\Documents\Newsletters\Summer2018.pdf.gz")
+
+    assert "C:\\Documents\\Newsletters\\Summer2018" == drop_extension(
+        "C:\\Documents\\Newsletters\\Summer2018.pdf.gz")
