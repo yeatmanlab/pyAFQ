9616677cd4b80d13ed495df6779836f094121bae
36000 <jk6.28@outlook.com>
2022-07-11 14:43:59 -0700

    update test and clarify dependent situation

--- AFQ/tasks/utils.py
+++ AFQ/tasks/utils.py
@@ -7,17 +7,25 @@ __all__ = ["get_fname", "with_name"]
 def get_fname(base_fname, suffix,
               tracking_params=None, segmentation_params=None):
     fname = base_fname
+    using_trk = False
+    using_seg = False
     if tracking_params is not None and 'odf_model' in tracking_params:
         odf_model = tracking_params['odf_model']
         directions = tracking_params['directions']
         fname = fname + (
-            "_dependent-trk"
             f'_space-RASMM_model-{odf_model}'
             f'_desc-{directions}'
         )
+        using_trk = True
     if segmentation_params is not None and 'seg_algo' in segmentation_params:
         seg_algo = segmentation_params['seg_algo']
-        fname = f'{fname}_dependent-rec_algo-{seg_algo}'
+        using_seg = True
+        fname = fname + f"_algo-{seg_algo}"
+
+    if using_seg:
+        fname = fname + "_dependent-rec"
+    elif using_trk:
+        fname = fname + "_dependent-trk"
 
     return fname + suffix
 
