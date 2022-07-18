d37915907b44702c1fd2a74e416c15079e1e1ba9
36000 <jk6.28@outlook.com>
2022-07-18 15:23:45 -0700

    fix indices, ouput sls_bundle_assignment instead

--- AFQ/segmentation.py
+++ AFQ/segmentation.py
@@ -766,6 +766,9 @@ class Segmentation:
         tg = StatefulTractogram(tg.streamlines[possible_fibers],
                                 self.img,
                                 Space.VOX)
+        if self.return_idx:
+            out_idx = out_idx[possible_fibers]
+
         streamlines_in_bundles = streamlines_in_bundles[possible_fibers]
         min_dist_coords = min_dist_coords[possible_fibers]
         if self.roi_dist_tie_break:
@@ -775,8 +778,9 @@ class Segmentation:
 
         if self.save_intermediates is not None:
             os.makedirs(self.save_intermediates, exist_ok=True)
-            bc_path = op.join(self.save_intermediates, "bundle_choice.npy")
-            np.save(bc_path, bundle_choice)
+            bc_path = op.join(self.save_intermediates,
+                              "sls_bundle_assignment.npy")
+            np.save(bc_path, streamlines_in_bundles)
 
         # We do another round through, so that we can orient all the
         # streamlines within a bundle in the same orientation with respect to
