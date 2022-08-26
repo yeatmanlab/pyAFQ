6c1afabf9f3b8896a6fd2e369fe0e442393c9518
36000 <jk6.28@outlook.com>
2022-08-26 14:34:45 -0700

    this should be true

--- examples/vof_example.py
+++ examples/vof_example.py
@@ -16,7 +16,7 @@ import AFQ.utils.streamlines as aus
 
 import os.path as op
 
-afd.organize_stanford_data(clear_previous_afq=False)
+afd.organize_stanford_data(clear_previous_afq=True)
 
 bundle_names = ["ARC_L", "ARC_R", "pARC_L", "pARC_R", "VOF_L", "VOF_R"]
 bundle_dict = abd.BundleDict(bundle_names)
