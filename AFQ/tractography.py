375adf494948d85f061b5277c4fe09efc9e5a5a6
36000 <jk6.28@outlook.com>
2022-09-19 13:30:31 -0700

    [FIX] correctly calculate min / max sl length, update step_size docs

--- AFQ/tractography.py
+++ AFQ/tractography.py
@@ -77,7 +77,7 @@ def track(params_file, directions="prob", max_angle=30., sphere=None,
         Defaults to 0 (this means that if no stop_mask is passed,
         we will stop only at the edge of the image).
     step_size : float, optional.
-        The size (in mm) of a step of tractography. Default: 0.5
+        The size of a step (in voxels) of tractography. Default: 0.5
     min_length: int, optional
         The miminal length (mm) in a streamline. Default: 40
     max_length: int, optional
@@ -111,6 +111,13 @@ def track(params_file, directions="prob", max_angle=30., sphere=None,
     affine = params_img.affine
     odf_model = odf_model.upper()
     directions = directions.lower()
+
+    # We need to calculate the size of a voxel, so we can transform
+    # from mm to voxel units:
+    R = affine[0:3, 0:3]
+    vox_dim = np.mean(np.diag(np.linalg.cholesky(R.T.dot(R))))
+    min_length = min_length / vox_dim
+    max_length = max_length / vox_dim
 
     logger.info("Generating Seeds...")
     if isinstance(n_seeds, int):
