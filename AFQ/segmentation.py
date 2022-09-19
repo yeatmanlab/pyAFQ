e7f9e2539211f8e4be7e1bbb1580238cc02103b3
36000 <jk6.28@outlook.com>
2022-09-19 14:01:31 -0700

    stronger clean parameters

--- AFQ/segmentation.py
+++ AFQ/segmentation.py
@@ -1055,7 +1055,7 @@ class Segmentation:
         return fiber_groups
 
 
-def clean_bundle(tg, n_points=100, clean_rounds=5, distance_threshold=5,
+def clean_bundle(tg, n_points=100, clean_rounds=20, distance_threshold=3,
                  length_threshold=4, min_sl=20, stat='mean',
                  return_idx=False):
     """
@@ -1071,10 +1071,10 @@ def clean_bundle(tg, n_points=100, clean_rounds=5, distance_threshold=5,
         Default: 100
     clean_rounds : int, optional.
         Number of rounds of cleaning based on the Mahalanobis distance from
-        the mean of extracted bundles. Default: 5
+        the mean of extracted bundles. Default: 20
     distance_threshold : float, optional.
         Threshold of cleaning based on the Mahalanobis distance (the units are
-        standard deviations). Default: 5.
+        standard deviations). Default: 3.
     length_threshold: float, optional
         Threshold for cleaning based on length (in standard deviations). Length
         of any streamline should not be *more* than this number of stdevs from
