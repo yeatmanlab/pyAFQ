bea2cb1b62076e4ee8223bf2155338af2f7e6c20
36000 <jk6.28@outlook.com>
2023-06-14 16:17:47 -0700

    [ENH] add CUDA 12 compat to GPU tractography

--- gpu_docker/docker-build.sh
+++ gpu_docker/docker-build.sh
@@ -1,11 +1,21 @@
 COMMIT=${1}
 COMMIT="$(echo "${COMMIT}" | tr -d '[:space:]')"
 export COMMIT
-NO_TAG="ghcr.io/${2}/pyafq_gpu"
+
+NO_TAG="ghcr.io/${2}/pyafq_gpu_cuda_11"
 TAG="${NO_TAG}:${COMMIT}"
 TAG2="${NO_TAG}:latest"
 TAG="$(echo "${TAG}" | tr -d '[:space:]')"
 TAG2="$(echo "${TAG2}" | tr -d '[:space:]')"
-
 echo $TAG
 docker build --no-cache -t $TAG -t $TAG2 --build-arg COMMIT ./gpu_docker
+
+NVIDIAVERSION="12.0.1-devel-ubuntu20.04"
+export NVIDIAVERSION
+NO_TAG="ghcr.io/${2}/pyafq_gpu_cuda_12"
+TAG="${NO_TAG}:${COMMIT}"
+TAG2="${NO_TAG}:latest"
+TAG="$(echo "${TAG}" | tr -d '[:space:]')"
+TAG2="$(echo "${TAG2}" | tr -d '[:space:]')"
+echo $TAG
+docker build --no-cache -t $TAG -t $TAG2 --build-arg COMMIT --build-arg NVIDIAVERSION ./gpu_docker
