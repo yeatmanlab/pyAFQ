9d4ca7136321f04d92b9935a2ee13a2e67816b5e
36000 <jk6.28@outlook.com>
2023-06-19 18:53:50 -0700

    [FIX] split GPU pyafq docker builds into differnet actions

--- gpu_docker/docker-build-12.sh
+++ gpu_docker/docker-build-12.sh
@@ -2,10 +2,12 @@ COMMIT=${1}
 COMMIT="$(echo "${COMMIT}" | tr -d '[:space:]')"
 export COMMIT
 
-NO_TAG="ghcr.io/${2}/pyafq_gpu_cuda_11"
+NVIDIAVERSION="12.0.1-devel-ubuntu20.04"
+export NVIDIAVERSION
+NO_TAG="ghcr.io/${2}/pyafq_gpu_cuda_12"
 TAG="${NO_TAG}:${COMMIT}"
 TAG2="${NO_TAG}:latest"
 TAG="$(echo "${TAG}" | tr -d '[:space:]')"
 TAG2="$(echo "${TAG2}" | tr -d '[:space:]')"
 echo $TAG
-docker build -t $TAG -t $TAG2 --build-arg COMMIT ./gpu_docker
+docker build -t $TAG -t $TAG2 --build-arg COMMIT --build-arg NVIDIAVERSION ./gpu_docker
