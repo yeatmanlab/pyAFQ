88bbfb4e701c9ad3c553173e20cfac1dc6934443
36000 <jk6.28@outlook.com>
2023-06-20 08:25:25 -0700

    attempt to fold all of these into one file

--- gpu_docker/docker-build.sh
+++ gpu_docker/docker-build.sh
@@ -2,10 +2,12 @@ COMMIT=${1}
 COMMIT="$(echo "${COMMIT}" | tr -d '[:space:]')"
 export COMMIT
 
-NO_TAG="ghcr.io/${2}/pyafq_gpu_cuda_11"
+NVIDIAVERSION=${3}
+export NVIDIAVERSION
+NO_TAG="ghcr.io/${2}/pyafq_gpu_cuda_${4}"
 TAG="${NO_TAG}:${COMMIT}"
 TAG2="${NO_TAG}:latest"
 TAG="$(echo "${TAG}" | tr -d '[:space:]')"
 TAG2="$(echo "${TAG2}" | tr -d '[:space:]')"
 echo $TAG
-docker build -t $TAG -t $TAG2 --build-arg COMMIT ./gpu_docker
+docker build -t $TAG -t $TAG2 --build-arg COMMIT --build-arg NVIDIAVERSION ./gpu_docker
--- gpu_docker/docker-push-12.sh
+++
@@ -1,4 +0,0 @@
-NO_TAG="ghcr.io/${1}/pyafq_gpu_cuda_12"
-NO_TAG="$(echo "${NO_TAG}" | tr -d '[:space:]')"
-
-docker push --all-tags $NO_TAG
