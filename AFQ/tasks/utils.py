f37774d892e58cdbc2803d72573cf30b3617e749
36000 <jk6.28@outlook.com>
2023-09-21 11:10:43 -0700

    [ENH] more specific bids entities and fixes to be bids compatible

--- AFQ/tasks/utils.py
+++ AFQ/tasks/utils.py
@@ -1,7 +1,25 @@
+from AFQ.utils.path import drop_extension
 import os.path as op
 import inspect
 
-__all__ = ["get_fname", "with_name"]
+__all__ = ["get_fname", "with_name", "get_base_fname"]
+
+
+def get_base_fname(output_dir, dwi_data_file):
+    # setup up path and base file name for outputs
+    # remove suffix and desc from dwi data file name
+    used_key_list = ["desc", "space", "to", "from"]
+    dwi_dfile_no_ext = op.join(
+        output_dir,
+        drop_extension(op.basename(dwi_data_file)))
+    fname = op.dirname(dwi_dfile_no_ext) + "/"
+    for key_val_pair in op.basename(dwi_dfile_no_ext).split("_"):
+        if "-" in key_val_pair:
+            key = key_val_pair.split("-")[0]
+            if key not in used_key_list:
+                fname = fname + key_val_pair + "_"
+    fname = fname[:-1]
+    return fname
 
 
 def get_fname(base_fname, suffix,
@@ -11,11 +29,11 @@ def get_fname(base_fname, suffix,
         odf_model = tracking_params['odf_model']
         directions = tracking_params['directions']
         fname = fname + (
-            f'_space-RASMM_model-{directions+odf_model}'
+            f'_coordsys-RASMM_trkmodel-{directions+odf_model}'
         )
     if segmentation_params is not None and 'seg_algo' in segmentation_params:
         seg_algo = segmentation_params['seg_algo']
-        fname = fname + f"_algo-{seg_algo}"
+        fname = fname + f"_segalgo-{seg_algo}"
 
     return fname + suffix
 
