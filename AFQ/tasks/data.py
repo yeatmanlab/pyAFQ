1c37aa45d19bd89add581838f87dff11a45c0eab
36000 <jk6.28@outlook.com>
2023-12-11 11:59:34 -0800

    tidy this up

--- AFQ/tasks/data.py
+++ AFQ/tasks/data.py
@@ -38,7 +38,8 @@ DIPY_GH = "https://github.com/dipy/dipy/blob/master/dipy/"
 
 @pimms.calc("data", "gtab", "img")
 def get_data_gtab(dwi, bval, bvec, min_bval=None,
-                  max_bval=None, filter_b=True, b0_threshold=50):
+                  max_bval=None, filter_b=True, b0_threshold=50,
+                  prefered_orientation="RAS"):
     """
     DWI data as an ndarray for selected b values,
     A DIPY GradientTable with all the gradient information,
@@ -60,15 +61,21 @@ def get_data_gtab(dwi, bval, bvec, min_bval=None,
     b0_threshold : int, optional
         The value of b under which
         it is considered to be b0. Default: 50.
+    prefered_orientation : str, optional
+        pyAFQ reorients the data into this orientation.
+        Note that the GPU tractography only works with RAS.
+        Default: "RAS"
     """
     img = nib.load(dwi)
     bvals, bvecs = read_bvals_bvecs(bval, bvec)
 
     voxel_order = "".join(nib.orientations.aff2axcodes(img.affine))
-    if not voxel_order == "RAS":
+    if not voxel_order == prefered_orientation:
         # this code adapted from qsiprep.interfaces.images
-        input_orientation = nib.orientations.axcodes2ornt(voxel_order)
-        desired_orientation = nib.orientations.axcodes2ornt("RAS")
+        input_orientation = nib.orientations.axcodes2ornt(
+            voxel_order)
+        desired_orientation = nib.orientations.axcodes2ornt(
+            prefered_orientation)
         transform_orientation = nib.orientations.ornt_transform(
             input_orientation, desired_orientation)
         img = img.as_reoriented(transform_orientation)
